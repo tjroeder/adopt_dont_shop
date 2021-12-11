423c886d042bcc9f79ebcab90dd4046ff4c68da4
Tim Roeder <timothyjroeder@gmail.com>
2021-12-11 12:57:54 -0700

    Feat: add #set_defaults, #full_address

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,7 +1,20 @@
 class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+  after_initialize :set_defaults
 
   validates_associated :pet_applications
   validates :name, :street_address, :city, :state, :zip_code, :description, :status, presence: true
+
+  # Class Methods
+
+  # Instance Methods
+  def set_defaults
+    self.description ||= 'Pet Application Request Description'
+    self.status ||= 'In Progress'
+  end
+
+  def full_address
+    "#{street_address} #{city}, #{state} #{zip_code}"
+  end    
 end
