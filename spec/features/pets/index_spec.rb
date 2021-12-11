e1c31ada8f1aa74776a3a3239fb36aee6a49d78d
Tim Roeder <timothyjroeder@gmail.com>
2021-12-11 16:34:56 -0700

    Feat: dry up instance objects to use let!

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -1,41 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'the pets index' do
-  it 'lists all the pets with their attributes' do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+  let!(:shelter_1) { Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9) }
+  let!(:shelter_2) { Shelter.create!(name: 'Dumb Friends League', city: 'Denver, CO', foster_program: true, rank: 6) }
+  let!(:shelter_3) { Shelter.create!(name: 'Animal House', city: 'Fort Collins, CO', foster_program: false, rank: 9) }
 
-    visit "/pets"
+  let!(:pet_1) { shelter_1.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald') }
+  let!(:pet_2) { shelter_3.pets.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster') }
+  let!(:pet_3) { shelter_3.pets.create!(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven') }
+  let!(:pet_4) { shelter_2.pets.create!(adoptable: false, age: 5, breed: 'dachshund', name: 'Woodrow') }
+
+  let!(:application_1) { Application.create!(name: 'Tim', street_address: '123 Taco Lane', city: 'Fort', state: 'CO', zip_code: '12345') }
+  let!(:application_2) { Application.create!(name: 'Sue', street_address: '321 Burrito Lane', city: 'Boulder', state: 'UT', zip_code: '54321') }
+  let!(:application_3) { Application.create!(name: 'Case', street_address: '4231 Chili Lane', city: 'Denver', state: 'NY', zip_code: '33333') }
+
+  it 'lists all the pets with their attributes' do
+    visit pets_path
 
     expect(page).to have_content(pet_1.name)
     expect(page).to have_content(pet_1.breed)
     expect(page).to have_content(pet_1.age)
-    expect(page).to have_content(shelter.name)
+    expect(page).to have_content(shelter_1.name)
 
     expect(page).to have_content(pet_2.name)
     expect(page).to have_content(pet_2.breed)
     expect(page).to have_content(pet_2.age)
-    expect(page).to have_content(shelter.name)
+    expect(page).to have_content(shelter_3.name)
   end
 
   it 'only lists adoptable pets' do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
-    pet_3 = Pet.create(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: shelter.id)
-
-    visit "/pets"
+    visit pets_path
 
     expect(page).to_not have_content(pet_3.name)
+    expect(page).to_not have_content(pet_4.name)
   end
 
   it 'displays a link to edit each pet' do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
-
-    visit '/pets'
+    visit pets_path
 
     expect(page).to have_content("Edit #{pet_1.name}")
     expect(page).to have_content("Edit #{pet_2.name}")
@@ -46,39 +47,34 @@ RSpec.describe 'the pets index' do
   end
 
   it 'displays a link to delete each pet' do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
-
-    visit '/pets'
+    visit pets_path
 
     expect(page).to have_content("Delete #{pet_1.name}")
     expect(page).to have_content("Delete #{pet_2.name}")
 
     click_link("Delete #{pet_1.name}")
 
-    expect(page).to have_current_path("/pets")
+    expect(page).to have_current_path(pets_path)
     expect(page).to_not have_content(pet_1.name)
   end
 
   it 'has a text box to filter results by keyword' do
-    visit "/pets"
+    visit pets_path
     expect(page).to have_button("Search")
   end
 
   it 'lists partial matches as search results' do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
-    pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
+    pet_5 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter_1.id)
 
-    visit "/pets"
+    visit pets_path
 
     fill_in 'Search', with: "Ba"
     click_on("Search")
 
     expect(page).to have_content(pet_1.name)
-    expect(page).to have_content(pet_2.name)
+    expect(page).to have_content(pet_5.name)
+    expect(page).to_not have_content(pet_2.name)
     expect(page).to_not have_content(pet_3.name)
+    expect(page).to_not have_content(pet_4.name)
   end
 end
