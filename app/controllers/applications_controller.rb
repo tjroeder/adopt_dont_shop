7bfcf274ff45b5d369e11f73d75fe3fa411b20c0
Tim Roeder <timothyjroeder@gmail.com>
2021-12-11 20:46:11 -0700

    Feat: add #new and #create actions

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationsController < ApplicationController
   before_action :set_application, only: [:show]
 
+  def new
+    @application = Application.new
+  end
+
+  def create
+    application = Application.create!(application_params)
+    redirect_to application_path(application)
+  end
+
   def show
   end
 
@@ -8,5 +17,9 @@ class ApplicationsController < ApplicationController
 
   def set_application
     @application = Application.find(params[:id])
+  end
+
+  def application_params
+    params.require(:application).permit(:name, :street_address, :city, :state, :zip_code)
   end
 end
